36b186a07f7d7d32c18ac0fcd3e7e5315a4c8079
luanacamaral2012 <luanacamaral2012@gmail.com>
2025-05-26 17:20:17 +0000

    criação da função para remover os itens do array: removeItems

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
+    if (itemName === "") {
+        alert("Digite um item válido!")
+        return
+    }
 
     const item = {
         name: itemName,
@@ -22,16 +26,40 @@ function showItemsList(){
         sectionList.innerHTML += `
             <div class="item">
                 <div>
-                    <input type="checkbox" name="List" id="item-${index}">
-                    <div class="custom-checkbox">
+                    <input type="checkbox" name="List" id="item-${index}" ${item.checked &&"checked"}>
+                    <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                         <img src="./assets/checked.svg" alt="checked">
                     </div>
                     <label for="item-${index}">${item.name}</label>
                 </div>
-                <button>
+                <button onclick="removeItem('${item.name}')">
                     <img src="./assets/trash-icon.svg" alt="trash-icon">
                 </button>
             </div>
         `
     }) 
+} 
+    function checkItem(itemName) {
+        const item = items.find((item) => item.name === itemName)
+       item.checked = !item.checked
+       showItemsList()
+    }
+
+ 
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+
+        if(itemIndex !== -1){
+        items.splice(itemIndex, 1)
+        }
+    showItemsList()
+} 
+
+
+function addHideWarningClass() {
+    document.querySelector(".warning").classList.add("hide-warning")
 }
+
